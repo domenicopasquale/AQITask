f72ef39c284cb48fcd0d51000ea7b4bcf809f904
Domenico Pasquale <domenicopasq@gmail.com>
2021-03-25 11:56:49 +0100

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
+
+module.exports = {
+    entry: "./js/main.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "app.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node-modules)/,
+                include: path.resolve(__dirname, "src")
+            }
+        ]
+    },
+    plugins: [
+        new Dotenv({
+            path: "./.env"
+        })
+    ]
+};
