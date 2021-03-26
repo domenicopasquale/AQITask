cedbf208042962bac6f384b898d6004ec8e92af0
Domenico Pasquale <domenicopasq@gmail.com>
2021-03-26 10:06:58 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: "./js/main.js",
     mode: "development",
     output: {
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "public"),
         filename: "app.js"
     },
     module: {
